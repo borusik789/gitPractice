79a8784e66e5322494ec34c4f26649da24591e92
borusik789 <borusik789@gmail.com>
2020-08-02 09:24:45 +0300

    first commit

--- src/Login.java
+++ src/Login.java
@@ -3,6 +3,10 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("why?");
+        System.out.println("ME");
+
+
+
 
     }
 }
