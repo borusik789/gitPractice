73e2123957e1e98ff114f3536c14d53a28b79993
borusik789 <borusik789@gmail.com>
2020-08-02 09:12:43 +0300

    Login

--- src/Login.java
+++ src/Login.java
@@ -3,6 +3,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("why?");
-        System.out.println("Boris");
+
     }
 }
